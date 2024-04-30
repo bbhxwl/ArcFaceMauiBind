8ce332d51cdc9e9b4c5fe41f207e3c51bd338680
bbhxwl <470138890@qq.com>
2024-04-30 13:50:55 +0800

    11

---
+++ ArcFaceMauiBind/Platforms/Android/ArcHelper.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ArcFaceMauiBind;
+
+public class ArcHelper
+{
+    public static int ActiveOnline(string? appId, string? sdkKey)
+    {
+        return Com.Arcsoft.Face.FaceEngine.ActiveOnline(Platform.CurrentActivity, appId, sdkKey);
+    }
+}
